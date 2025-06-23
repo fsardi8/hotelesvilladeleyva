070905d8b7f1d3aa1deefe2bc8283a5dd63f69c6
Felipe Sardi <felipe@redsi.co>
2025-06-23 18:52:40 -0500

    prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** prettier.config.js */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
